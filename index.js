23de438a3df8ce7ce7a638d070dcf63714a8d566
Jacob Schmocker <jacobcs1996@gmail.com>
2018-01-11 23:58:54 -0600

    implement basic timer

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 const player = require('play-sound')(opts = {});
+const clearScreen = require('./util/clearScreen');
 
-player.play('output.mp3', (err) => {
-    if (err) console.log(err);
-    console.log('Session ended!')
-});
+function printTimeLeft(duration, elapsed) {
+    const minutes = Math.floor((duration - elapsed) / 60);
+    let seconds = Math.floor((duration - elapsed) % 60);
+
+    // Make sure that (minutes):00 is printed when seconds === 0
+    seconds = (seconds !== 0) ? seconds : '00';
+
+    console.log(`${minutes}:${seconds}`);
+}
+
+function meditationTimer(duration, callback) {
+    const durationInSeconds = duration * 60;
+    let timeElapsed = 0;
+
+    clearScreen();
+    printTimeLeft(durationInSeconds, timeElapsed);
+
+    let timer = setInterval(() => {
+        timeElapsed += 1;
+        if (timeElapsed == durationInSeconds) {
+            clearInterval(timer);
+            console.log(`The timer has finished. You meditated for ${timeElapsed / 60} minutes. `);
+            if (callback) callback();
+        }
+        clearScreen();
+        printTimeLeft(durationInSeconds, timeElapsed);
+    }, 1000);
+}
+
+meditationTimer(1.33);
