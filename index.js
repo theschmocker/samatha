e1d933910caf54a3ec43609fce68125790f06b57
Jacob Schmocker <jacobcs1996@gmail.com>
2018-01-15 17:47:32 -0600

    refactored MeditationTimer into its own module

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
 #! /usr/bin/env node
+const MeditationTimer = require('./timer');
 const player = require('play-sound')(opts = {});
-const cli = require('./util/cli');
-const clear = require('clear');
-
 const config = require('./config.json');
 
 function alarm() {
     player.play(config.settings.sound);
 }
 
-function meditationTimer(duration, callback) {
-    const durationInSeconds = duration * 60;
-    let timeElapsed = 0;
-
-    cli.clearScreen();
-    cli.printTimeLeft(durationInSeconds, timeElapsed);
-
-    let timer = setInterval(() => {
-        timeElapsed += 1;
-        if (timeElapsed == durationInSeconds) {
-            clearInterval(timer);
-            clear();
-            console.log(`The timer has finished. You meditated for ${timeElapsed / 60} ${(timeElapsed / 60) === 1 ? 'minute' : 'minutes'}`);
-            if (callback) callback();
-            return;
-        }
-        clear();
-        cli.printTimeLeft(durationInSeconds, timeElapsed);
-    }, 1000);
-}
-
 const time = process.argv[2] || config.settings.timerDuration;
-meditationTimer(time, alarm);
+
+MeditationTimer.start(time, alarm);
+
+// Exit gracefully when user exits with Ctrl-c
+process.on('SIGINT', () => MeditationTimer.end());
